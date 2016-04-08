9a3f2e2d74107750d3a6700f56dad39335ed6bdf
carlos <carmacias@uan.edu.co>
2016-04-08 09:20:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/carlos/taller4carapapa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.carlos.taller4carapapa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
